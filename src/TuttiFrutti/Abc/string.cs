8c36af47a962cacbb8eed30d452a778aa842b5c7
Kiryl Drougakov <kiryl_d@gmx.net>
2025-12-26 11:57:58 +0100

    @string: spaces extended

--- src/TuttiFrutti/Abc/string.cs
+++ src/TuttiFrutti/Abc/string.cs
@@ -1,12 +1,20 @@
 ﻿namespace Abc;
 public class @string
 {
-    public static string Space => " ";
+    public const string Space = " ";
     public static string Spaces(ushort num) => 0 == num ? string.Empty : new(' ', num);
 }
 
 public class Spaces { 
-    public string Two => "  ";
-    public string Three => "  ";
-    public string Four => "  ";
+    public const string Two = @string.Space + @string.Space;
+    public string Three => Two + @string.Space;
+    public string Four => Three + @string.Space;
+    public string Five => Four + @string.Space;
+    public string Six => Five + @string.Space;
+    public string Seven => Six + @string.Space;
+    public string Eight => Seven + @string.Space;
+    public string Nine => Eight + @string.Space;
+    public string Ten => Nine + @string.Space;
+    public string Eleven => Ten + @string.Space;
+    public string Twelve => Eleven + @string.Space;
 }
